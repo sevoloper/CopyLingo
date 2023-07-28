0a8bf43f06e2afbdfef020ea93d146007c1b48e2
sevoloper <sevvalulku62@gmail.com>
2023-07-28 09:14:10 +0300

    Add project files.

---
+++ CopyLingo.Web/Shared/TempShared/WordDto.cs
@@ -0,0 +1,9 @@
+﻿namespace CopyLingo.Shared.TempShared
+{
+    public class WordDto
+    {
+        public int Id { get; set; }
+        public string English { get; set; }
+        public string Turkish { get; set; }
+    }
+}
